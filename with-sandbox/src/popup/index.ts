e31bdb45676d858157b052fe58aafb91dcca447f
zhang <xbzhang2020@icloud.com>
2025-01-31 13:31:46 +0800

    feat: add sidepanel

---
+++ with-sandbox/src/popup/index.ts
@@ -0,0 +1,12 @@
+import "./index.css";
+
+const rootEl = document.querySelector("#root");
+if (rootEl) {
+  rootEl.innerHTML = `
+  <div class="content">
+    <h1>Vanilla WebExtend</h1>
+    <p>This is a web page.</p>
+    <iframe id="sandboxFrame1" src="sandbox.html"></iframe>
+  </div>
+  `;
+}
