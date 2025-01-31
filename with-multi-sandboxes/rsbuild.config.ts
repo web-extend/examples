cdce436df043a98d906b38e2d1ca39e07807855f
zhang <xbzhang2020@icloud.com>
2025-01-31 13:25:39 +0800

    feat: add sandboxes

--- with-multi-sandboxes/rsbuild.config.ts
+++ with-multi-sandboxes/rsbuild.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "@rsbuild/core";
+// import { pluginWebExtend } from '@web-extend/rsbuild-plugin';
 import { pluginWebExtend } from "../../web-extend/packages/rsbuild/src/index";
-// import { pluginWebExtend } from "@web-extend/rsbuild-plugin";
 
 export default defineConfig({
   plugins: [pluginWebExtend()],
