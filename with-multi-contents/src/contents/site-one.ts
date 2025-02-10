a70adb1fbb437c0d18b2ffb5b6ad75e0c1aceead
zhang <xbzhang2020@icloud.com>
2025-02-11 07:55:40 +0800

    chore: multiple-content

--- with-multi-contents/src/contents/site-one.ts
+++ with-multi-contents/src/contents/site-one.ts
@@ -1,7 +1,13 @@
 import './site-one.css';
 
-console.log('one');
+console.log('three');
 
 export const config = {
   matches: ['https://www.baidu.com/'],
+  world: "MAIN"
 };
+
+// support hmr in dev mode
+if (module.hot) {
+  module.hot.accept();
+}
