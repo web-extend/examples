e3a5ddefa4b2ea69616f9a8d4017e15228af053e
zhangxingbin <xbzhang2020@icloud.com>
2025-07-17 09:06:34 +0800

    chore: add type for webExt config

--- with-react/web-ext.config.ts
+++ with-react/web-ext.config.ts
@@ -1,5 +1,7 @@
-export default {
+import { defineWebExtConfig } from "web-extend";
+
+export default defineWebExtConfig({
   run: {
     startUrl: "https://developer.mozilla.org/en-US/docs/Web/HTML",
   },
-};
+});
