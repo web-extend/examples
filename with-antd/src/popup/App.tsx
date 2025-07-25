e1897b0e4f50a44d22cabbed972d6980b4ce6821
zhangxingbin <xbzhang2020@icloud.com>
2025-07-25 23:58:07 +0800

    chore: count button

--- with-antd/src/popup/App.tsx
+++ with-antd/src/popup/App.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="content">
-      <h1>WebExtend + React</h1>
-      <p>This is a web page.</p>
+      <p>Count: {count}</p>
+      <Button type="primary" onClick={() => setCount(count + 1)}>
+        Click me
+      </Button>
     </div>
   );
 }
